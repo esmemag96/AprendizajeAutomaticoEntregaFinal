72dd561af65153e05c32f9892ab17f65f967696e
emilianoabascal <emilianoabascal@me.com>
2020-05-07 20:27:06 -0500

    gRAFICA CULERA

--- static/app.js
+++ static/app.js
@@ -84,43 +84,55 @@ window.onload = function () {
     getGraphs(-1);
 };
 
+// function setChart(data) {
+//     console.log(data['arr1']);
+//     var ctx = document.getElementById('myChart').getContext('2d');
+//     var stackedLine = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//             labels: data['arr1'],
+//             datasets: [{
+//                 data: data['arr1'],
+//                 label: "arr1",
+//                 borderColor: "#3e95cd",
+//                 fill: false
+//             }, {
+//                 data: data['arr2'],
+//                 label: "arr2",
+//                 borderColor: "#8e5ea2",
+//                 fill: false
+//             }
+//             ]
+//         },
+//         options: {
+//             title: {
+//                 display: true,
+//                 text: 'World population per region (in millions)'
+//             }
+//         }
+//     });
+// }
+
 function setChart(data) {
-    console.log(data);
+    console.log(data['arr1']);
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    new Chart(document.getElementById("myChart"), {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [data['arr1'], data['arr2']],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+          labels: data['arr1'],
+          datasets: [
+            {
+              label: "eXAMPO",
+              backgroundColor: "#3e95cd",
+              data: data['arr1']
+            }
+          ]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+          title: {
+            display: true,
+            text: 'Population growth (millions)'
+          }
         }
     });
 }
