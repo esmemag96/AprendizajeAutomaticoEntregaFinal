8b30653b7b9e6aeecb543276696aa0f1fba6d643
antony999k <lv999k@hotmail.com>
2020-05-10 14:02:20 -0500

    :sparkles: navBar component added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './main-components/home/home.component';
 import { DashboardComponent } from './main-components/dashboard/dashboard.component';
 import { NotFoundComponent } from './main-components/not-found/not-found.component';
+import { NavBarComponent } from './main-components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
