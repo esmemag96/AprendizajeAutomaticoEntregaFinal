6f515b3056a276e60d98c7c57c5bbeb184ca3413
esmeralda-m <esmeralda.m@ibm.com>
2020-05-11 13:32:24 -0500

    predict service started

--- Frontend/src/app/main-components/home/home.component.ts
+++ Frontend/src/app/main-components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ScoreService } from '../../services/score.service';
+import { Score } from '../../model/Score';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  scoreForm: FormGroup;
 
+  constructor(private scoreService: ScoreService,
+  ) { }
   ngOnInit() {
+    // this.scoreForm = this.formBuilder.group({
+    //   id: [''],
+    //   grade1: [''],
+    //   grade2: ['']
+    // });
   }
 
-}
+  get isInvalid() {
+    return this.scoreForm.invalid;
+  }
+  get f() {
+    return this.scoreForm.controls;
+  }
+
+  submitLogin(): void {
+    if (this.isInvalid) {
+      return;
+    }
+
+    const score: Score = {
+      TeacherID: this.f.id.value,
+      grade1: this.f.grade1.value,
+      grade2: this.f.grade2.value,
+    };
+
+
+    this.scoreService.score(score).subscribe(loginResponse => {
+      console.log(loginResponse);
+
+    });
+  }
+} 
---
+++ Frontend/src/app/model/Score.ts
@@ -0,0 +1,5 @@
+export interface Score {
+    'TeacherID': string, 
+    '1stGrade': string , 
+    '2ndGrade': string
+}
