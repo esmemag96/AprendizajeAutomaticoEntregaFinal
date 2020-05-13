024b0b39ade45b901e044833bcfe09584349e796
antony999k <lv999k@hotmail.com>
2020-05-12 19:03:59 -0500

    :sparkles: Text analize render

--- Frontend/src/app/main-components/analize-text/analize-text.component.ts
+++ Frontend/src/app/main-components/analize-text/analize-text.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { TextService } from '../../services/text.service';
-import { Text } from '../../model/Text';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { TextService } from "../../services/text.service";
+import { Text } from "../../model/Text";
 
 @Component({
-  selector: 'app-analize-text',
-  templateUrl: './analize-text.component.html',
-  styleUrls: ['./analize-text.component.scss']
+  selector: "app-analize-text",
+  templateUrl: "./analize-text.component.html",
+  styleUrls: ["./analize-text.component.scss"],
 })
 export class AnalizeTextComponent implements OnInit {
-
   textForm: FormGroup;
-  result: string;
-  constructor(private textService: TextService, private formBuilder: FormBuilder,
+  resultScore: string;
+  resultKeys: Array<string>;
+  // Handle Loading
+  isLoadingAnalisis: boolean;
+  // Handle Error
+  analisisError: boolean;
+  errorMessage: boolean;
 
-  ) { }
+  constructor(
+    private textService: TextService,
+    private formBuilder: FormBuilder
+  ) {
+    this.isLoadingAnalisis = false;
+
+    this.analisisError = false;
+  }
   ngOnInit() {
     this.textForm = this.formBuilder.group({
-      input: [''],
+      input: ["", Validators.required],
     });
   }
 
@@ -26,13 +37,34 @@ export class AnalizeTextComponent implements OnInit {
   }
 
   submitText(): void {
+    this.isLoadingAnalisis = true;
+    this.analisisError = false;
+    this.errorMessage = null;
 
     const text: Text = {
       inputText: this.f.input.value,
     };
 
-    this.textService.text(text).subscribe(predictResponse => {
-      console.log(predictResponse);
-    });
+    this.textService.text(text).subscribe(
+      (predictResponse) => {
+        this.isLoadingAnalisis = false;
+        this.resultScore = predictResponse[0].documents[0].score;
+        this.resultScore = String(Math.round((Number(this.resultScore) * 100) * 100)/100);
+        this.resultKeys = predictResponse[1].documents[0].keyPhrases
+        console.log(predictResponse);
+        console.log(this.resultKeys);
+      },
+      (err) => {
+        this.isLoadingAnalisis = false;
+        this.analisisError = true;
+        this.errorMessage = err.statusText;
+
+        setTimeout(() => {
+          this.analisisError = false;
+          this.errorMessage = null;
+        }, 3000);
+        console.log("HTTP Error", err);
+      }
+    );
   }
 }
